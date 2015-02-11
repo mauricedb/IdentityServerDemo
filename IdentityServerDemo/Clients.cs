44765a4d6818dec6b78a28c729b0a3080010d65b
Maurice de Beijer <mauricedb@computer.org>
2015-02-11 10:50:13 +0100

    Start

---
+++ IdentityServerDemo/Clients.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thinktecture.IdentityServer.Core.Models;
+
+namespace IdentityServerDemo
+{
+    public class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new List<Client>
+           {
+               new Client()
+               {
+                   ClientName = "MVC Client",
+                   ClientId = "mvc"
+               }
+           };
+        }
+    }
+}
